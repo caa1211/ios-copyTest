6e8e43882aa1cc7e12317f7f770b474ccd710c75
caa1211 <caa@livemail.tw>
2017-01-26 01:12:14 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ios-copyTest
+# ios-copyTest
--- tet/ViewController.m
+++ tet/ViewController.m
@@ -12,11 +12,32 @@
 
 @end
 
+
+@interface Person : NSObject
+@property (nonatomic) NSArray *bookArray1;
+@property (copy, nonatomic) NSArray *bookArray2;
+@end
+
+@implementation Person
+-(void)setBookArray2:(NSArray *)bookArray2 {
+    _bookArray2 = [bookArray2 copy];
+}
+@end
+
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    NSMutableArray *sourceArray = [@[@[@"book1"]] mutableCopy];
+    Person *p = [[Person alloc] init];
+    p.bookArray1 = sourceArray;
+    p.bookArray2 = sourceArray;
+    
+    [sourceArray addObject:@"book2"];
+    NSLog(@"");
+    
 }
 
 - (void)didReceiveMemoryWarning {
