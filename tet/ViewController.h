0786ef4a74470fad29cbfcab9ce3a5bde0428277
caa1211 <caa@livemail.tw>
2017-01-26 00:59:02 +0800

    Initial Commit

---
+++ tet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tet
+//
+//  Created by caa on 1/26/17.
+//  Copyright © 2017 c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tet
+//
+//  Created by caa on 1/26/17.
+//  Copyright © 2017 c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
